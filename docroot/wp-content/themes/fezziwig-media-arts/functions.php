95334e4fc6cc8702c151de10c44e3dbe48c945c5
Richard Gilbert <gilbert67@gmail.com>
2024-07-24 18:31:48 -0700

    Function to populate custom field with NGG galleries.

--- docroot/wp-content/themes/fezziwig-media-arts/functions.php
+++ docroot/wp-content/themes/fezziwig-media-arts/functions.php
@@ -192,3 +192,24 @@ add_filter( 'get_the_archive_title', function( $title ) {
 	}
 	return $title;
 });
+
+/**
+ * Simplify the title of category pages (removes "Category:").
+ */
+function fezziwig_media_arts_gallery_choices( $field ) {
+	// reset choices
+	$field['choices'] = array();
+	// get all NextGen galleries
+	global $wpdb;
+	$galleries = $wpdb->get_results( "SELECT * FROM {$wpdb->prefix}ngg_gallery" );
+	// loop through galleries and add to choices
+	if ( ! empty( $galleries ) ) {
+			foreach ( $galleries as $gallery ) {
+					$field['choices'][ $gallery->gid ] = $gallery->name;
+			}
+	}
+	// return the field
+	return $field;
+}
+add_filter('acf/load_field/name=gallery_id', 'fezziwig_media_arts_gallery_choices');
+
