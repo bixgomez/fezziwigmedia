19d168cd8fb5757419f8ccdfe7cf8206601f8712
Richard Gilbert <gilbert67@gmail.com>
2024-08-29 10:15:56 -0400

    Updating post teasers template

--- docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teasers.php
+++ docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teasers.php
@@ -11,13 +11,21 @@ if( $category_id ) :
         <ul class="post-teasers">
             <?php while( $query->have_posts() ) : $query->the_post(); ?>
                 <li>
+                    <?php if( has_post_thumbnail() ) : ?>
+                        <a href="<?php the_permalink(); ?>">
+                            <?php the_post_thumbnail('thumbnail'); ?>
+                        </a>
+                    <?php endif; ?>
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                     <p><?php echo wp_trim_words( get_the_excerpt(), 20, '...' ); ?></p>
                 </li>
             <?php endwhile; ?>
         </ul>
         <?php wp_reset_postdata(); ?>
+    <?php else : ?>
+        <p>No posts found in this category.</p>
     <?php endif;
 else :
     echo '<p>No category selected.</p>';
 endif;
+?>
