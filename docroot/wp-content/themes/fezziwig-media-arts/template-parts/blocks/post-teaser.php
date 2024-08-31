54ad9059ae14e67edfd147188d58e2e029ff921f
Richard Gilbert <gilbert67@gmail.com>
2024-08-31 08:39:18 -0400

    Attempts to display ACF field in post teaser.

--- docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teaser.php
+++ docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teaser.php
@@ -10,4 +10,29 @@
   <p class="post-teaser-text">
     <?php echo get_the_excerpt(); ?>
   </p>
+
+  <?php
+  // Get the external link ACF field
+
+  echo '<pre>';
+  print_r(get_field('external_link'));
+  echo '</pre>';
+
+  the_field('external_link');
+
+  $external_link = get_field('external_link');
+  var_dump($external_link);
+  if ( $external_link ) :
+    $link_url = $external_link['url']; // Get the URL
+    $link_text = $external_link['title'] ? $external_link['title'] : $link_url; // Use URL if no title
+    $link_target = $external_link['target'] ? $external_link['target'] : '_self'; // Get target, default to _self
+    ?>
+  hello
+    <p class="external-link">
+      <a href="<?php echo esc_url($link_url); ?>" target="<?php echo esc_attr($link_target); ?>" rel="noopener noreferrer">
+        <?php echo esc_html($link_text); ?>
+      </a>
+    </p>
+  <?php endif; ?>
+  goodbye
 </div>
--- docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teasers.php
+++ docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teasers.php
@@ -2,24 +2,29 @@
 echo '<!-- post-teasers.php -->';
 $category_id = get_field('post_teasers_category');
 if( $category_id ) :
-    $args = array(
-        'cat' => $category_id,
-        'posts_per_page' => 5,
-    );
-    $query = new WP_Query( $args );
-    if( $query->have_posts() ) : ?>
-        <ul class="post-teasers">
-          <?php while( $query->have_posts() ) : $query->the_post(); ?>
-          <li>
-            <?php get_template_part('template-parts/blocks/post-teaser'); ?>
-          </li>
-          <?php endwhile; ?>
-        </ul>
-        <?php wp_reset_postdata(); ?>
-    <?php else : ?>
-        <p>No posts found in this category.</p>
-    <?php endif;
+  $category = get_category($category_id); // Get the category object
+  ?>
+  <h2><?php echo esc_html($category->name); ?></h2> <!-- Display category name -->
+  <p><?php echo esc_html($category->description); ?></p> <!-- Display category description -->
+  <?php
+  $args = array(
+    'cat' => $category_id,
+    'posts_per_page' => 5,
+  );
+  $query = new WP_Query( $args );
+  if( $query->have_posts() ) : ?>
+    <ul class="post-teasers">
+      <?php while( $query->have_posts() ) : $query->the_post(); ?>
+        <li>
+          <?php get_template_part('template-parts/blocks/post-teaser'); ?>
+        </li>
+      <?php endwhile; ?>
+    </ul>
+    <?php wp_reset_postdata(); ?>
+  <?php else : ?>
+    <p>No posts found in this category.</p>
+  <?php endif;
 else :
-    echo '<p>No category selected.</p>';
+  echo '<p>No category selected.</p>';
 endif;
 ?>
