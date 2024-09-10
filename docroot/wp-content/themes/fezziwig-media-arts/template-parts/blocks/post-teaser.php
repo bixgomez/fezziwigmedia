e5ae91c2899bd2e470cb864b78dd69e7a0ff7cc2
Richard Gilbert <gilbert67@gmail.com>
2024-09-09 20:41:55 -0700

    Reformatting.

--- docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teaser.php
+++ docroot/wp-content/themes/fezziwig-media-arts/template-parts/blocks/post-teaser.php
@@ -1,16 +1,16 @@
-
-
 <!-- post-teaser.php -->
-
 <div class="post-teaser">
-  <h3 class="post-teaser-title">
+  
+<h3 class="post-teaser-title">
     <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
   </h3>
+  
   <?php if( has_post_thumbnail() ) : ?>
     <div class="post-teaser-image">
       <?php the_post_thumbnail('thumbnail'); ?>
     </div>
   <?php endif; ?>
+  
   <p class="post-teaser-text">
     <?php echo get_the_excerpt(); ?>
   </p>
@@ -28,4 +28,5 @@
       </a>
     </p>
   <?php endif; ?>
+
 </div>
