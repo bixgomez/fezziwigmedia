194887a8600ea3819a807e760ba0a667f5f29a39
Richard Gilbert <gilbert67@gmail.com>
2025-07-10 21:29:01 -0700

    Placing the WordPress chosen page as the home page.

--- fezziwig-vue/src/views/Home.vue
+++ fezziwig-vue/src/views/Home.vue
@@ -1,6 +1,29 @@
 <template>
-  <div>
-    <h1>Welcome</h1>
-    <p>This is the home page of my Vue + WordPress site.</p>
+  <div v-if="page">
+    <h1 v-html="page.title.rendered" />
+    <div v-html="page.content.rendered" />
+  </div>
+  <div v-else>
+    <p>Loading...</p>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      page: null
+    }
+  },
+  async mounted() {
+    // First, get the site settings
+    const settingsResponse = await fetch('https://fezziwigmedia.ddev.site/wp-json/')
+    const settings = await settingsResponse.json()
+    
+    // Then fetch the actual home page by ID
+    const pageResponse = await fetch(`https://fezziwigmedia.ddev.site/wp-json/wp/v2/pages/${settings.page_on_front}`)
+    this.page = await pageResponse.json()
+  }
+}
+</script>
