9084ce7343266326c892e22fe1a280280e1f0891
Richard Gilbert <gilbert67@gmail.com>
2025-08-21 08:19:41 -0700

    Add title to single view

--- fezziwig-vue/src/views/Single.vue
+++ fezziwig-vue/src/views/Single.vue
@@ -1,5 +1,6 @@
 <template>
-  <div v-if="blocks.length">
+  <div v-if="blocks.length || pageTitle">
+    <h1 v-if="pageTitle" v-html="pageTitle" />
     <component
       v-for="(block, index) in blocks"
       :key="index"
@@ -21,6 +22,7 @@ import RawHtmlBlock from '@/components/RawHtmlBlock.vue'
 
 const blocks = ref([])
 const slug = ref('')
+const pageTitle = ref('')
 
 function resolveComponent(blockName) {
   switch (blockName) {
@@ -39,12 +41,23 @@ onMounted(async () => {
   slug.value = window.location.pathname.split('/').filter(Boolean).pop()
   const apiBase = import.meta.env.VITE_API_BASE_URL
 
-  // 1) hit our custom proxy endpoint for raw block comments
-  const res = await fetch(`${apiBase}/wp-json/fezziwig/v1/blocks/${slug.value}`)
-  const { blocks: raw } = await res.json()
-
-  // 2) parse them into structured block objects
+  // 1) Fetch page data for title
   try {
+    const pageRes = await fetch(`${apiBase}/wp-json/wp/v2/pages?slug=${slug.value}`)
+    const [pageData] = await pageRes.json()
+    if (pageData) {
+      pageTitle.value = pageData.title.rendered
+    }
+  } catch (e) {
+    console.error('Page fetch error:', e)
+  }
+
+  // 2) hit our custom proxy endpoint for raw block comments
+  try {
+    const res = await fetch(`${apiBase}/wp-json/fezziwig/v1/blocks/${slug.value}`)
+    const { blocks: raw } = await res.json()
+
+    // 3) parse them into structured block objects
     blocks.value = parse(raw)
     console.log('Parsed blocks:', blocks.value)
   } catch (e) {
